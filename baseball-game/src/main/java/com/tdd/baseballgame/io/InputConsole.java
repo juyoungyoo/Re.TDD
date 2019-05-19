febd1ae477d8f19d86d032fcb6195ff94be4050b
Juyoung <juy5790@outlook.com>
2019-05-19 15:29:42 +0900

    io 수정

--- baseball-game/src/main/java/com/tdd/baseballgame/io/InputConsole.java
+++ baseball-game/src/main/java/com/tdd/baseballgame/io/InputConsole.java
@@ -7,12 +7,13 @@ public class InputConsole {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String writeAnswer(){
-        System.out.println("========정답을 작성하세요=========");
+        System.out.println("========================");
+        System.out.println("정답을 입력하세요.");
         return scanner.nextLine();
     }
 
     public static String ask() {
-        System.out.println("========예상되는 정답을 작성하세요=========");
+        System.out.println("예상되는 답을 입력하세요.");
         return scanner.nextLine();
     }
 }
--- baseball-game/src/main/java/com/tdd/baseballgame/io/OutputConsole.java
+++ baseball-game/src/main/java/com/tdd/baseballgame/io/OutputConsole.java
@@ -13,22 +13,22 @@ public class OutputConsole {
 
     public static void answer(Answer answer) {
         Numbers numbers = answer.getNumbers();
-
         List<Digit> digits = numbers.getDigits();
-        System.out.println(":::::: Digit :::::::");
+        System.out.println("------------------------");
+        System.out.print("DIGIT : ");
         for (Digit digit : digits) {
-            System.out.print(digit.getNumber() + "\t");
+            System.out.print(digit.getNumber() + " ");
         }
-        System.out.println("\n::::::::::::::::::::");
+        System.out.println("\n------------------------");
     }
 
     public static void result(AnswerResult answerResult) {
-        System.out.println(":::::: TURN ::::::: " + answerResult.getTryCount());
-        System.out.println("::::::: RESULT ::::::: ");
+        System.out.println("------------------------");
+        System.out.println("TURN [" + answerResult.getTryCount() + "]");
         Map<ResultType, Integer> resultType = answerResult.getResult();
         for(Map.Entry<ResultType, Integer> result : resultType.entrySet()){
             System.out.println(result.getKey().name() + ": " + result.getValue());
         }
-        System.out.println("::::::::::::::::::::::: ");
+        System.out.println("------------------------");
     }
 }
